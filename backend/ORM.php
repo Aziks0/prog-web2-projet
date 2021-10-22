5bfcd49f284fdda813ff9c1965e39ac3195beafe
Benjamin DIDIER <36425380+Aziks0@users.noreply.github.com>
2021-10-22 19:29:19 +0200

    feat: ORM exception handling

--- backend/ORM.php
+++ backend/ORM.php
@@ -4,30 +4,47 @@ class ORM
 {
     private PDO $pdo;
 
+    /**
+     * @throws Exception
+     */
     public function __construc()
     {
-        $this->pdo = new PDO('sqlite:' . dirname(__FILE__) . '/database.db');
+        try {
+            $this->pdo = new PDO('sqlite:' . dirname(__FILE__) . '/database.db');
+            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $ex) {
+            echo 'Connexion failed: ' . $ex->getMessage();
+            throw new Exception('Connexion failed');
+        }
 
         $this->createTables();
     }
 
     /**
      * Create a users and an articles table if they do not exist
+     * 
+     * @throws Exception
      */
     private function createTables(): void
     {
-        $this->pdo->query('CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username VARCHAR(255) NOT NULL UNIQUE,
-            password VARCHAR(255) NOT NULL
-        )');
+        try {
+            $this->pdo->query('CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL
+            )');
 
-        $this->pdo->query('CREATE TABLE IF NOT EXISTS articles (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title VARCHAR(100) NOT NULL,
-            body TEXT NOT NULL,
-            category VARCHAR(20) NOT NULL
-        )');
+            $this->pdo->query('CREATE TABLE IF NOT EXISTS articles (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title VARCHAR(100) NOT NULL,
+                body TEXT NOT NULL,
+                category VARCHAR(20) NOT NULL
+            )');
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+            throw new Exception('Failed to create tables');
+        }
     }
 
     /**
@@ -36,14 +53,21 @@ class ORM
      * @param string $title The title of the article
      * @param string $body The body of the article
      * @param string $category The category of the article
+     * 
+     * @throws Exception
      */
     public function insertArticle(string $title, string $body, string $category): void
     {
-        $statement = $this->pdo->prepare('INSERT INTO articles (title, body, category) VALUES (:title, :body, :category)');
-        $statement->bindValue(':title', $title);
-        $statement->bindValue(':body', $body);
-        $statement->bindValue(':category', $category);
-        $statement->execute();
+        try {
+            $statement = $this->pdo->prepare('INSERT INTO articles (title, body, category) VALUES (:title, :body, :category)');
+            $statement->bindValue(':title', $title);
+            $statement->bindValue(':body', $body);
+            $statement->bindValue(':category', $category);
+            $statement->execute();
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+            throw new Exception('Failed to insert the article');
+        }
     }
 
     /**
@@ -51,10 +75,17 @@ class ORM
      * 
      * @return array An array containing all articles, the articles are arrays
      * indexed by their column name, or an empy array if there are no article
+     * 
+     * @throws Exception
      */
     public function fetchArticles(): array
     {
-        return $this->pdo->query('SELECT * FROM articles')->fetchAll(PDO::FETCH_ASSOC);
+        try {
+            return $this->pdo->query('SELECT * FROM articles')->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+            throw new Exception('Failed to fetch the articles');
+        }
     }
 
     /**
@@ -63,15 +94,22 @@ class ORM
      * @param string $username The username to check
      * 
      * @return bool true if the username is in the database, false otherwise
+     * 
+     * @throws Exception
      */
     public function isUsernameInDatabase(string $username): bool
     {
-        $statement = $this->pdo->prepare('SELECT COUNT(*) FROM users WHERE username = :username');
-        $statement->bindValue(':username', $username);
-        $statement->execute();
-        $rowCount = (int) $statement->fetchColumn();
+        try {
+            $statement = $this->pdo->prepare('SELECT COUNT(*) FROM users WHERE username = :username');
+            $statement->bindValue(':username', $username);
+            $statement->execute();
+            $rowCount = (int) $statement->fetchColumn();
 
-        return $rowCount == 0 ? false : true;
+            return $rowCount == 0 ? false : true;
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+            throw new Exception();
+        }
     }
 
     /**
@@ -79,13 +117,20 @@ class ORM
      * 
      * @param string $username The username of the user
      * @param string $password The hashed password of the user
+     * 
+     * @throws Exception
      */
     public function insertUser(string $username, string $password): void
     {
-        $statement = $this->pdo->prepare('INSERT INTO users (username, password) VALUES (:username, :password)');
-        $statement->bindValue(':username', $username);
-        $statement->bindValue(':password', $password);
-        $statement->execute();
+        try {
+            $statement = $this->pdo->prepare('INSERT INTO users (username, password) VALUES (:username, :password)');
+            $statement->bindValue(':username', $username);
+            $statement->bindValue(':password', $password);
+            $statement->execute();
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+            throw new Exception('Failed to insert the user');
+        }
     }
 
     /**
@@ -95,13 +140,20 @@ class ORM
      * 
      * @return array An array containing the id and the hashed password of the
      * user indexed by their column name, or an empty array if the user is not in the database
+     * 
+     * @throws Exception
      */
     public function fetchUser(string $username): array
     {
-        $statement = $this->pdo->prepare('SELECT id, password FROM users WHERE username = :username');
-        $statement->bindValue(':username', $username);
-        $statement->execute();
+        try {
+            $statement = $this->pdo->prepare('SELECT id, password FROM users WHERE username = :username');
+            $statement->bindValue(':username', $username);
+            $statement->execute();
 
-        return $statement->fetch(PDO::FETCH_ASSOC);
+            return $statement->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+            throw new Exception('Failed to fetch the user');
+        }
     }
 }
