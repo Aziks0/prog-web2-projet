60bf2a55e13b9f2066fcb40307f51eb46284af20
Benjamin DIDIER <36425380+Aziks0@users.noreply.github.com>
2021-11-10 18:43:47 +0100

    feat: add header

---
+++ frontend/header.php
@@ -0,0 +1,11 @@
+<header class="header">
+    <div class="header__content">
+        <a id="header__brand" href="#">Critics</a>
+        <nav class="header__nav">
+            <a href="#">Accueil</a>
+            <a href="#">Articles</a>
+            <a href="#">À propos</a>
+        </nav>
+        <a href="#">Se connecter</a>
+    </div>
+</header>
