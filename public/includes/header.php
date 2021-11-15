935024cfaec6211100de2c8851422531ce479ced
Benjamin DIDIER <36425380+Aziks0@users.noreply.github.com>
2021-11-15 19:17:38 +0100

    refactor: move session_start() out of header

--- public/includes/header.php
+++ public/includes/header.php
@@ -7,16 +7,12 @@
             <a href="#">À propos</a>
         </nav>
         <div class="header__session__container">
-            <?php
-            session_start();
-            if (isset($_SESSION['username'])) :
-            ?>
+            <?php if (isset($_SESSION['username'])) : ?>
                 <a href="/prog-web2-projet/public/publish">Écrire un article</a>
                 <a href="/prog-web2-projet/public/logout">Se déconnecter</a>
             <?php else : ?>
                 <a href="/prog-web2-projet/public/login">Se connecter</a>
             <?php endif; ?>
-
         </div>
     </div>
 </header>
