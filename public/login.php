04440eec8bf02e7f6db442f72826fac24ccfc961
Benjamin DIDIER <36425380+Aziks0@users.noreply.github.com>
2021-11-29 19:51:44 +0100

    feat: add css for better browser compatibility

--- public/login.php
+++ public/login.php
@@ -14,6 +14,7 @@ if (isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Critics</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/header.css">
 </head>
 
